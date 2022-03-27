5022e633ed3dfc8e3791992d07e63f0304554dbc
Atick Faisal <atickfaisal@gmail.com>
2022-03-27 09:26:47 +0600

    Add permission checking extension

--- core/src/main/kotlin/dev/atick/core/utils/extensions/ContextExtensions.kt
+++ core/src/main/kotlin/dev/atick/core/utils/extensions/ContextExtensions.kt
@@ -1,7 +1,9 @@
 package dev.atick.core.utils.extensions
 
 import android.content.Context
+import android.content.pm.PackageManager
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import dev.atick.core.BuildConfig
 
 fun Context.showToast(message: String) {
@@ -12,4 +14,9 @@ fun Context.debugMessage(error: String) {
     if (BuildConfig.DEBUG) {
         this.showToast(error)
     }
+}
+
+fun Context.hasPermission(permissionType: String): Boolean {
+    return ContextCompat.checkSelfPermission(this, permissionType) ==
+        PackageManager.PERMISSION_GRANTED
 }
