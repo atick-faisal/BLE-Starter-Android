c9df03dc86bfd95c94a2db3b56cb292222521621
Atick Faisal <atickfaisal@gmail.com>
2022-03-27 23:59:46 +0600

    Refactor

--- ble/src/main/kotlin/dev/atick/ble/repository/BLEManager.kt
+++ ble/src/main/kotlin/dev/atick/ble/repository/BLEManager.kt
@@ -6,11 +6,14 @@ import dev.atick.ble.data.BLEDevice
 import kotlinx.coroutines.flow.Flow
 
 interface BLEManager {
+
     fun initializeBluetooth(context: Context)
+
     fun enableBluetooth(
         activity: ComponentActivity,
         onSuccess: () -> Unit = {}
     )
+
     fun scanDevices(
         activity: ComponentActivity,
         autoConnect: Boolean
